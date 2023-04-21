de6aa85efd44cddf500efda4b6c3ad4f2be63bce
VladBorisof <vl-borisof@yandex.ru>
2023-04-21 22:16:14 +0300

    Query Parameters: Параметры которые указываются через ?

--- main.py
+++ main.py
@@ -2,37 +2,30 @@ from enum import Enum
 
 from fastapi import FastAPI
 
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 app = FastAPI()
 
 
-@app.get('/users/me')
-async def read_user_me():
-    return {"user_id": "the current user"}
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+        user_id: int,
+        item_id: str,
+        q: str | None = None,
+        short: bool = False
+) -> dict:
+    item = {'item_id': item_id, 'owner_id': user_id}
+    if q:
+        item.update({'q': q})
+    if not short:
+        item.update(
+            {'description': 'This is an amazing item that has a long description'}
+        )
+    return item
 
 
-@app.get('/users/{item_id}')
-async def read_item(item_id: int):
-    return {"user_id": item_id}
-
-
-@app.get('/models/{model_name}')
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == 'lenet':
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
+@app.get('/items/{item_id}')
+async def get_item(item_id: str, needy: str, skip: int = 0, limit: int = 10) -> dict:
+    item = {'item_id': item_id, 'needy': needy, 'skip': skip, 'limit': limit}
+    item.update(fake_items_db[0])
+    return item
