00daff22cad21a94c6ddf8fd785b4145cc3717d6
VladBorisof <vl-borisof@yandex.ru>
2023-04-26 22:27:26 +0300

    Query Parameters and String Validations: Generic validations and metadata:
    alias
    title
    description
    deprecated
    Validations specific for strings:
    
    min_length
    max_length
    regex
    In these examples you saw how to declare validations for str values.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
 
 
@@ -12,18 +14,21 @@ class Item(BaseModel):
 app = FastAPI()
 
 
-@app.post('/items/')
-async def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict |= {"price_with_tax": price_with_tax}
-    return item_dict
-
-
-@app.put('/items/{item_id}')
-async def create_item(item_id: int, item: Item, q: str | None = None):
-    result = {"item_id": item_id, **item.dict()}
+@app.get('/items/')
+async def create_item(
+        q: Annotated[
+            list[str] | None,
+            Query(
+                alias="item-query",
+                title="Query string",
+                description="Query string for the items to search in the database that have a good match",
+                min_length=3,
+                max_length=50,
+                regex="^fixedquery$",
+                deprecated=True,
+            )] = None
+) -> dict:
+    result = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
-        result.update({"q": q})
+        result |= {"q": q}
     return result
